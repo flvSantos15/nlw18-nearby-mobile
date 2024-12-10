94c05a7b0c95a6c92dfb13d081a0b0a9872b186c
flvsantos15 <flvsantos300@gmail.com>
2024-12-09 22:55:37 -0300

    feat(steps): add steps component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,14 @@
+import { View } from 'react-native'
+
+import { Steps } from '@/components/steps'
 import { Welcome } from '@/components/welcome'
-import { View, Text } from 'react-native'
 
 export default function Home() {
   return (
     <View style={{ flex: 1, padding: 40, gap: 40 }}>
       <Welcome />
+
+      <Steps />
     </View>
   )
 }
